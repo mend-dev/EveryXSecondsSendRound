7d19873e988bfcc8a92cccd595624f8d04c21d00
mend-dev <mendbookdev@gmail.com>
2023-05-18 17:11:50 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,74 @@
+﻿using HarmonyLib;
+using Il2CppAssets.Scripts.Models.Rounds;
+using Il2CppAssets.Scripts.Simulation;
+using Il2CppAssets.Scripts.Simulation.Towers;
+using Il2CppAssets.Scripts.Unity;
+using Il2CppAssets.Scripts.Unity.Bridge;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using Il2CppAssets.Scripts.Unity.UI_New.Popups;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using MelonLoader;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+using Il2CppAssets.Scripts.Utils;
+using UnityEngine.PlayerLoop;
+
+namespace EveryXSecondsSendRound {
+    public class Main : MelonMod {
+
+        private bool gameStarted = false;
+
+        private int waitTime = 30;
+        private int maxNextRounds = 3;
+
+        [System.Obsolete]
+        public override void OnApplicationStart() {
+            base.OnApplicationStart();
+            //Logger.Log("Voice Activation Has Loaded");
+        }
+
+        public override void OnUpdate() {
+            base.OnUpdate();
+
+            if (InGame.instance != null) {
+                if (InGame.instance.bridge != null) {
+                    if (Input.GetKeyDown(KeyCode.F6)) {
+                        PopupScreen.instance.ShowSetValuePopup("Time Between Rounds", "Sets the time (seconds) between sending new rounds",
+                            new Action<int>(i => {
+                                if (i < 1) { i = 1; }
+                                if (i > 60) { i = 60; }
+                                waitTime = i;
+                            }), 1);
+                    } else if (Input.GetKeyDown(KeyCode.F7)) {
+                        PopupScreen.instance.ShowSetValuePopup("Round Max", "Sets the max number of rounds that can be sent every X seconds",
+                            new Action<int>(i => {
+                                if (i < 1) { i = 1; }
+                                if (i > 10) { i = 10; }
+                                maxNextRounds = i;
+                            }), 3);
+                    }
+
+                    if (!gameStarted) {
+                        gameStarted = true;
+                        Task task = new Task(Wait);
+                        task.Start();
+                    }
+                }
+            }
+        }
+
+        private void Wait() {
+            Thread.Sleep(waitTime * 1000);
+            Random rand = new Random();
+            int rounds = rand.Next(maxNextRounds) + 1;
+            for (int i = 0; i < rounds; i++) {
+                InGame.instance.bridge.SendRaceRound();
+            }
+            Wait();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EveryXSecondsSendRound")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EveryXSecondsSendRound")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(EveryXSecondsSendRound.Main), "EveryXSecondsSendRound", "1.0.0", "menddev")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a16049f7-6c9e-45ac-82a4-c20f9830c6a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
